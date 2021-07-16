46ad6b59508751941807b6018a9b934cc0bf1b7b
Sergey Tereshchenko <serghh1988@gmail.com>
2021-07-16 15:25:44 +0300

    Написал логику и стили кнопки дозагрузки изображений. Подключил библиотеку уведомлений react-hot-toast.

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import ImageGallery from './components/ImageGallery';
 import Modal from './components/Modal';
 
 import { AppContainer } from './App.styles';
+import LoadMoreButton from './components/Button';
 
 export default class App extends Component {
   state = {
@@ -23,31 +24,43 @@ export default class App extends Component {
     showModal: false,
     tags: '',
     selectedImage: '',
-    loader: false,
     error: null,
   };
 
-  async componentDidUpdate(_, prevState) {
+  componentDidUpdate(_, prevState) {
     const { imageName, page } = this.state;
-
-    const shouldFetch = prevState.imageName !== imageName && imageName !== '';
+    const shouldFetch =
+      prevState.imageName !== imageName || prevState.page !== page;
     if (shouldFetch) {
-      try {
-        this.setState({ reqStatus: 'pending', images: [] });
-        const images = await fetchImages(page, imageName);
-        this.setState({
-          images,
-          reqStatus: 'resolved',
-          page: prevState.page + 1,
-        });
-      } catch (error) {
-        this.setState({ reqStatus: 'rejected' });
-        toast.error('Ooops, there is no such image');
-      }
+      this.setState({ reqStatus: 'pending' });
+      this.onFetchImages();
+    }
+
+    if (page > 1) {
+      this.scrollPageToEnd();
+    }
+  }
+
+  async onFetchImages() {
+    const { imageName, page } = this.state;
+    try {
+      const images = await fetchImages(page, imageName);
+      this.setState(prevState => ({
+        images: [...prevState.images, ...images],
+        reqStatus: 'resolved',
+      }));
+    } catch (error) {
+      this.setState({ reqStatus: 'rejected', error: true });
+      toast.error('Ooops, there is no such image');
     }
   }
 
   handleFormSubmit = imageName => {
+    if (imageName.trim() === '') {
+      toast.error('Invalid search query');
+      return;
+    }
+    this.resetState();
     this.setState({ imageName });
   };
 
@@ -60,16 +73,45 @@ export default class App extends Component {
     this.toggleModal();
   };
 
+  scrollPageToEnd = () => {
+    setTimeout(() => {
+      window.scrollTo({
+        top: document.documentElement.scrollHeight,
+        behavior: 'smooth',
+      });
+    }, 1000);
+  };
+
+  onLoadMoreBtn = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
+  resetState = () => {
+    this.setState({
+      imageName: null,
+      page: 1,
+      images: [],
+      status: 'idle',
+    });
+  };
+
   render() {
-    const { images, showModal, selectedImage, reqStatus } = this.state;
+    const { images, showModal, selectedImage, reqStatus, error } = this.state;
+    const showImages = images.length > 11;
+
     return (
       <AppContainer>
+        {error && toast.error('No such pictures on the server!')}
         <Searchbar onSearch={this.handleFormSubmit} />
         {reqStatus === 'pending' && <Loader />}
         <ImageGallery onSelect={this.handleSelectedImage} images={images} />
+        {showImages && <LoadMoreButton onClick={this.onLoadMoreBtn} />}
         {showModal && (
           <Modal onClose={this.toggleModal} largeImageURL={selectedImage} />
         )}
+        <Toaster position="top-right" />
       </AppContainer>
     );
   }
